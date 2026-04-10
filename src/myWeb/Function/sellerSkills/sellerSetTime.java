daa12f84578f5b10b16e915bd0d488d45bb2ba7b
nguyendodat8 <nguyendodat8@gmail.com>
2026-04-10 18:12:31 +0700

    <Build> thêm các hàm cho interface sellerSetTime

--- src/myWeb/Function/sellerSkills/sellerSetTime.java
+++ src/myWeb/Function/sellerSkills/sellerSetTime.java
@@ -1,5 +1,10 @@
 package myWeb.Function.sellerSkills;
 
+import java.time.LocalDateTime;
+
 public interface sellerSetTime {
+    void setTime(LocalDateTime start, LocalDateTime end);
+    void extendDuration(int minutes);
+    void getRemainingTime();
 }
 
