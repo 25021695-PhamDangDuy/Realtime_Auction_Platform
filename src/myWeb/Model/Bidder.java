4984f8c4a08433c329b5314eca44e505481f1869
Pham_Dang_Duy <25021695@vnu.edu.vn>
2026-04-17 20:19:02 +0700

    <Moved> di chuyển class Bidder và Deposit về Models

--- src/myWeb/Model/Bidder.java
+++ src/myWeb/Model/Bidder.java
@@ -1,13 +1,14 @@
-package myWeb.Bidder;
-
-import myWeb.Model.User;
+package myWeb.Model;
 
 public class Bidder extends User {
     private double balance=0;
+
     public Bidder(String id,String name,String password){
         super(id,name,password);
     }
+
     void setBalance(double bal){
         balance+=bal;
     }
 }
+
--- src/myWeb/Model/Deposit.java
+++ src/myWeb/Model/Deposit.java
@@ -1,10 +1,12 @@
-package myWeb.Bidder;
-import java.util.Scanner;
+package myWeb.Model;
+
 public class Deposit {
+
     public static void deposit(Bidder bidder){
-        System.out.println("Nhập số tiền cần nạp");
-        Scanner sc=new Scanner(System.in);
+//        System.out.println("Nhập số tiền cần nạp");
+//        Scanner sc=new Scanner(System.in);
         double addMoney=Double.parseDouble(sc.nextLine());
         bidder.setBalance(addMoney);
     }
+
 }
