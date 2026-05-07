ccd3a10be55f6ecebfc15e8cd68703a02f8e62b2
nguyendodat8 <nguyendodat8@gmail.com>
2026-05-07 10:47:13 +0700

    <Build> Tạo các giao diện: Đăng ký, Đăng nhập, Sản phẩm đấu giá

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module Realtime.Auction.Platform {
     requires javafx.controls;
     requires javafx.graphics;
+    opens myWeb.view to javafx.fxml; // Thay đổi theo package của bạn
+    exports myWeb.view;
+
 }
