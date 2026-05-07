05450fccc6719c60c12759989b81d6d4cd0d290e
Ngô Đình Dũng <ngodinhdungbn@gmail.com>
2026-05-07 10:52:37 +0700

    Fix

--- src/main/java/myWeb/controller/AuctionSession.java
+++ src/main/java/myWeb/controller/AuctionSession.java
@@ -1,17 +1,23 @@
 package myWeb.controller;
 
 public class AuctionSession {
-    String produceId;
-    double currentPrice;//giá hiện tại
-    double reservePrice;//giá khởi điểm
-    String topBidder;//người thắng cuộc.
-    AuctionStatus state;//trạng thái
+    private String produceId;
+    private Item item;
+    private Seller seller;
 
-    public AuctionSession(String produceId,double reservePrice) {
+    private double currentPrice;//giá hiện tại
+    private double minIncreament; // bước giá tối thiểu
+    private Bidder topBidder; //người trả giá cao nhất hiện tại
+
+    private LocalDateTime endtime;// thời gian kết thúc
+    private boolean isActive=true;// trạng thái phiên
+
+    public AuctionSession(String produceId,Seller seller,double startPrice,double minIncreament,LocalDateTime endtime) {
         this.produceId = produceId;
-        this.reservePrice = reservePrice;
-        this.currentPrice = reservePrice;
-        this.state = AuctionStatus.OPEN;
+        this.seller=seller;
+        this.currentPrice=startPrice;
+        this.minIncreament=minIncreament;
+        this.endtime=endtime;
 
     }
 
