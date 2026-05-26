a605a7d2e346f8119268607b9bd44871c30b560a
Pham_Dang_Duy <25021695@vnu.edu.vn>
2026-05-26 13:04:43 +0700

    Setup SQLite database

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module Realtime.Auction.Platform {
     requires javafx.controls;
     requires javafx.graphics;
+    requires java.sql;
     opens myWeb.view to javafx.fxml; // Thay đổi theo package của bạn
     exports myWeb.view;
 
