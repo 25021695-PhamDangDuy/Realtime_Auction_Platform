79ee3faa2ccd97b9d0a39c76edd653d5f1b2e4df
nguyendodat8 <nguyendodat8@gmail.com>
2026-05-28 18:43:49 +0700

    Tạo thêm các exception cho phiên đấu giá

---
+++ src/main/java/myWeb/exception/AuctionNotFoundException.java
@@ -0,0 +1,7 @@
+package myWeb.exception;
+
+public class AuctionNotFoundException extends Exception{
+    public AuctionNotFoundException(String msg){
+        super(msg);
+    }
+}
---
+++ src/main/java/myWeb/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package myWeb.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
